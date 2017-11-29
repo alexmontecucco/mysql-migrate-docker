19597ec77e7d762fdcba87665c8f98b194d5e18c
Alexandre Montecucco <alexandre.montecucco@grab.com>
2017-11-29 08:28:37 +0800

    handle bash files + proper indentation

--- sql-migrate.sh
+++ sql-migrate.sh
@@ -5,32 +5,35 @@
 # Remove all the lines between the migrate down section and either migrate up section or EOF
 # then pipes the result to mysql
 sql_migrate_clean() {
-    sed '/^-- +migrate Down/,/^-- +migrate Up/d' "$1" | ${mysql[@]}
+  sed '/^-- +migrate Down/,/^-- +migrate Up/d' "$1" | ${mysql[@]}
 }
 
 # Scan the dir looking for sql file and goes recursively through sub folders
 scan() {
-    DIR="$1"
-    SUB_DIR=()
+  DIR="$1"
+  SUB_DIR=()
 
-    echo "$0: running in dir: $DIR"
-    for f in "$DIR"/*; do
-        if [ -d "$f" ]; then
-            SUB_DIR+=("$f")
-        else
-            case "$f" in
-                *.sql)
-                    echo "$0: running $f"
-                    sql_migrate_clean "$f" ;;
-                *)
-                    echo "$0: ignoring $f" ;;
-             esac
-        fi
-	done
+  echo "$0: running in dir: $DIR"
+  for f in "$DIR"/*; do
+    if [ -d "$f" ]; then
+      SUB_DIR+=("$f")
+    else
+      case "$f" in
+      *.sh)
+        echo "$0: running $f"
+        . "$f" ;;
+      *.sql)
+        echo "$0: running $f"
+        sql_migrate_clean "$f" ;;
+      *)
+        echo "$0: ignoring $f" ;;
+      esac
+    fi
+  done
 
-	for d in "${SUB_DIR[@]}"; do
-	    scan "$d"
-	done
+  for d in "${SUB_DIR[@]}"; do
+    scan "$d"
+  done
 }
 
 scan /sql-data
